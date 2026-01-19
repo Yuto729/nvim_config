bb960e5600420bb256185a471d213da88bc66491
mitomi <mitomi.yuuto2003@gmail.com>
2026-01-19 14:12:46 +0900

    add other settings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Disable autoformat globally
+vim.g.autoformat = false
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
