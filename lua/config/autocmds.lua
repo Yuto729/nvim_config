dfa0c27d21c4d46a2164fb137d31cdaf2cc0634c
mitomi <mitomi.yuuto2003@gmail.com>
2026-01-22 00:34:29 +0900

    markdownの赤線を修正: スペルチェックの無効化によって解決

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,5 +5,7 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Markdownでのスペルチェックを無効化（日本語対応）
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
